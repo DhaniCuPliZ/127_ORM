c7bfa5a8d7720f525fbcccf455e38e8d2ba569bf
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2025-10-29 12:16:27 +0700

    menambahkan endpoint untuk menampilkan semua data komik

--- index.js
+++ index.js
@@ -32,3 +32,13 @@ app.post('/komik', async (req, res) => {
     res.status(500).send({ message: error.message });
   }
 });
+
+app.get('/komik', async (req, res) => {
+    try {
+        const komik = await db.Komik.findAll();
+        res.send(komik);
+    } catch (error) {
+        res.send({ message: error.message });
+    }
+});
+
